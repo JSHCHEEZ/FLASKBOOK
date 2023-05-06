e50a863d00467984d6f6429e4c89e00463c17159
JSHCHEEZ <zndn887@naver.com>
2023-05-06 10:35:49 +0900

    app.py 수정_2

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,8 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, current_app, g, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return "Hello, Cheeze"
+    return "Hello, FlaskBook"
+
+
+@app.route("/hello/<name>", methods=["GET", "POST"], endpoint="hello-endpoint")
+def hello(name):
+    return f"Hello, {name}!"
+
+
+@app.route("/name/<name>")
+def show_name(name):
+    return render_template("index.html", name=name)
+
+
+with app.test_request_context():
+    # /
+    print(url_for("index"))
+    # /hello/world
+    print(url_for("hello-endpoint", name="world"))
+    # /name/AK?page=1
+    print(url_for("show_name", name="AK", page="1"))
+
+ctx = app.app_context()
+ctx.push()
+
+
+print(current_app.name)
+
+g.connection = "connection"
+print(g.connection)
+
+with app.test_request_context("/users?updated=true"):
+    print(request.args.get("updated"))
